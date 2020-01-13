5305f425834d3943bfec2cb7e15c128e076fc49e
fabian <fabianjose99@gmail.com>
2020-01-13 12:26:59 -0500

    2 commit

--- GESTINV/Form1.Designer.cs
+++ GESTINV/Form1.Designer.cs
@@ -34,8 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1003, 687);
+            this.ClientSize = new System.Drawing.Size(1259, 838);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GESTINV";
             this.ResumeLayout(false);
 
--- GESTINV/Form1.cs
+++ GESTINV/Form1.cs
@@ -17,12 +17,11 @@ namespace GESTINV
         public Form1()
         {
             InitializeComponent();
-            // Create a material theme manager and add the form to manage (this)
+           
             MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
 
-            // Configure color schema
             materialSkinManager.ColorScheme = new ColorScheme(
                 Primary.Blue400, Primary.Blue500,
                 Primary.Blue500, Accent.LightBlue200,
